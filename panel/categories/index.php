37336a1e35cff55207f12729604168e6e22b2751
alejandro <alejandrorecio.r@gmail.com>
2020-10-31 20:11:46 +0100

    Change head to render function

--- panel/categories/index.php
+++ panel/categories/index.php
@@ -10,25 +10,7 @@ $families = $productfamilies->getList(" AND ID NOT IN ('" . implode("','", array
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Categorías | Panel de control</title>
-    <meta name="description" content="description here">
-    <meta name="keywords" content="keywords,here">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link href="https://unpkg.com/tailwindcss@next/dist/tailwind.min.css" rel="stylesheet">
-    <link href="../../assets/css/master.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/03a10de34c.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="../../assets/js/main.js"></script>
-
-
-
-</head>
+<?php $template->renderHeadPanel($productfamilies->_realPluralName); ?>
 
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
 
--- panel/index.php
+++ panel/index.php
@@ -6,28 +6,11 @@ include("config/config.php");
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home | Panel de control</title>
-    <meta name="description" content="description here">
-    <meta name="keywords" content="keywords,here">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
-        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link href="https://unpkg.com/tailwindcss@next/dist/tailwind.min.css" rel="stylesheet">
-    <!--Replace with your tailwind.css once created-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"
-        integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
-
-
-
-</head>
+<?php $template->renderHeadPanel("Home"); ?>
 
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
 
-    <?php $template->renderPanelMenu("categories");?>
+    <?php $template->renderPanelMenu(); ?>
 
     <!--Container-->
     <div class="container w-full mx-auto pt-20">
