bd4547a3b934b5a8e60419c3dc82c2a1953a385d
alejandro <alejandrorecio.r@gmail.com>
2020-11-01 13:05:19 +0100

    Set account object in templates

--- application/config.php
+++ application/config.php
@@ -12,4 +12,5 @@ function myAutoLoader($className) {
 }
 $host = array_shift((explode('.', $_SERVER['HTTP_HOST'])));
 $user = new User(1);
+$account = new Account($user->fields["ACCOUNT_ID"]);
 $template = new Template();
