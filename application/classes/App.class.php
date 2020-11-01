5d1f73ffed3d7deaef726bc2618222c630da51a8
alejandro <alejandrorecio.r@gmail.com>
2020-11-01 13:05:41 +0100

    Add now() function

--- application/classes/App.class.php
+++ application/classes/App.class.php
@@ -16,7 +16,7 @@ class App {
         $rs = CoreObject::conn()->query($query);
         $data = [];
         foreach ($rs as $v) {
-            $data[$v["CODE"]] = $v["NAME"];
+            $data[$v["ID"]] = $v["NAME"];
         }
 
         return $data;
@@ -24,6 +24,10 @@ class App {
 
     public static function getHost() {
         return "https://" . $_SERVER['HTTP_HOST'];
+    }
+
+    public static function now() {
+        return Date("Y-m-d H:i:s");
     }
 
 }
